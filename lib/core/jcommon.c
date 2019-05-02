e541b5425d67c80d81d0d9d4c23cfbcdf8c5debe
Michael Straßberger <3strassb@informatik.uni-hamburg.de>
2019-05-02 15:23:52 +0200

    Fix typo in error msg for smd loading

--- lib/core/jcommon.c
+++ lib/core/jcommon.c
@@ -192,7 +192,7 @@ j_init (void)
 	{
 		if (common->smd_backend == NULL || !j_backend_smd_init(common->smd_backend, smd_path))
 		{
-			J_CRITICAL("Could not initialize kv backend %s.\n", kv_backend);
+			J_CRITICAL("Could not initialize smd backend %s.\n", kv_backend);
 			goto error;
 		}
 	}
